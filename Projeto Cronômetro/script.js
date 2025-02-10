c098da5a737a10863623f9e2402b9c8c99d8b810
xSilas21 <97924180+xSilas21@users.noreply.github.com>
2025-02-10 16:42:35 -0300

    Add files via upload

---
+++ Projeto Cronômetro/script.js
@@ -0,0 +1,50 @@
+var display = document.getElementById('display');
+
+        var minutos = document.getElementById('minutos');
+        var segundos = document.getElementById('segundos');
+
+        var comecar = document.getElementById('comecar');
+
+
+        var cronometroSeg;
+
+        var minutoAtual;
+        var segundoAtual;
+
+        var interval;
+
+        for(var i = 0; i <= 60; i++){
+            minutos.innerHTML+='<option value="'+i+'">'+i+'</option>';
+        }
+
+        for(var i = 1; i <= 60; i++){
+            segundos.innerHTML+='<option value="'+i+'">'+i+'</option>';
+        }
+
+        comecar.addEventListener('click',function(){
+            minutoAtual = minutos.value;
+            segundoAtual = segundos.value;
+
+            display.childNodes[1].innerHTML = minutoAtual + ":"+segundoAtual;
+
+            interval = setInterval(function(){
+
+                segundoAtual--;
+                if(segundoAtual <= 0){
+                    if(minutoAtual > 0){
+                        minutoAtual--;
+                        segundoAtual = 59;
+                    }else{
+                        alert("Acabou!");
+                        document.getElementById("sound").play();
+                        clearInterval(interval);
+                    }
+                }
+                
+                display.childNodes[1].innerHTML = minutoAtual + ":"+segundoAtual;
+            },1000);
+
+
+        })
+
+        /* Feito e Desenvolvido por xSilas21 */
